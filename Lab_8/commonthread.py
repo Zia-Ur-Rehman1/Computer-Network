3b968f5caa50e0f5f7c368cd2649addc57c84d9a
Khan Alone <ziakhanalone100@gmail.com>
2021-06-15 20:05:18 +0500

    file response corected

--- Lab_8/commonthread.py
+++ Lab_8/commonthread.py
@@ -17,18 +17,16 @@ class commonThread(Thread):
         client_req=pickle.loads(self.clientsocket.recv(1024))
         client_resp=[]
         if(client_req=="0x0000"):
-            print("List req Recvied")
-            
             hex_string="0x0010"
             client_resp=onlyfiles.copy()
             client_resp.insert(0,len(onlyfiles))
             client_resp.insert(0,hex_string)
             self.clientsocket.send(pickle.dumps(client_resp))
-            print("List req sended")
             
         # File Req
         client_req=pickle.loads(self.clientsocket.recv(1024))
         print(client_req)
+        client_resp.clear()
         if(client_req[0]=="0x0001" and client_req[1] in onlyfiles):
             print("File req Recived")
             hex_string="0x0011"
@@ -36,6 +34,7 @@ class commonThread(Thread):
             client_resp.append(client_req[1])
             status=os.stat(join(path,client_req[1]))
             client_resp.append(status.st_size)
+            print(client_resp)
             self.clientsocket.send(pickle.dumps(client_resp))
             print("File req sended")
             
